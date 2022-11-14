3ab368df8974c554c23de786be6749fbb4d70eea
Collins Chemjor <collinschemjor@Collinss-MacBook-Pro-2.local>
2022-11-14 17:21:19 +0100

    feat: allow users to log out

--- lib/discuss_web/channels/comments_channel.ex
+++ lib/discuss_web/channels/comments_channel.ex
@@ -1,7 +1,10 @@
 defmodule DiscussWeb.CommentsChannel do
    use DiscussWeb, :channel
 
-   def join() do
+   def join(name, _auth_params, socket) do
+      IO.puts("+++++++++++++++++++++++")
+      IO.puts(name)
+      
    end
 
    def handle_in() do
