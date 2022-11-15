82c57e38ac9f1ef9c97d81427d9d4ecc327385bd
Collins Chemjor <collinschemjor@Collinss-MacBook-Pro-2.local>
2022-11-15 14:11:08 +0100

    Feat: load comments on dom loading, remove :gettext compiler

--- lib/discuss_web/channels/comments_channel.ex
+++ lib/discuss_web/channels/comments_channel.ex
@@ -3,12 +3,12 @@ defmodule DiscussWeb.CommentsChannel do
 
    @impl true
    def join(name, _params, socket) do
-      IO.puts("+++++++++++++++++++++++")
-      IO.puts(name)
       {:ok, %{hey: "there"}, socket}
    end
 
    @impl true
-   def handle_in(_param, _payload, _socket) do
+   def handle_in(name, message, socket) do
+
+      {:reply, :ok, socket}
    end
 end
